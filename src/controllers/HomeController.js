78a09bc7ae87494312a03665981cc0d47d50bad1
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-07-29 15:23:02 -0300

    Adicionando e configurando o Sequelize

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from "../models/Aluno";
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: "Davi",
+      sobrenome: "Ramos",
+      email: "davi.lima@ucsal.edu.br",
+      idade: 21,
+      peso: 70.0,
+      altura: 1.72,
     });
+    res.json(novoAluno);
   }
 }
 
