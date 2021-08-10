398d4bf4f0e8e5cb39aae8b7831d5297c221f249
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-08-10 18:31:41 -0300

    Ajeitando CORS, helmet e token expiresin

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -1,26 +1,26 @@
-import jwt from "jsonwebtoken";
-import User from "../models/User";
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
 class TokenController {
   async store(req, res) {
-    const { email = "", password = "" } = req.body;
+    const { email = '', password = '' } = req.body;
 
     if (!email || !password) {
       return res.status(401).json({
-        errors: ["Credenciais inválidas"],
+        errors: ['Credenciais inválidas'],
       });
     }
 
     const user = await User.findOne({ where: { email } });
     if (!user) {
       return res.status(401).json({
-        errors: ["Usuário não existe"],
+        errors: ['Usuário não existe'],
       });
     }
 
     if (!(await user.passwordIsValid(password))) {
       return res.status(401).json({
-        errors: ["Senha inválida"],
+        errors: ['Senha inválida'],
       });
     }
 
@@ -29,7 +29,7 @@ class TokenController {
       expiresIn: process.env.TOKEN_EXPIRATION,
     });
 
-    return res.json({ token });
+    return res.json({ token, user: { nome: user.nome, id, email } });
   }
 }
 
